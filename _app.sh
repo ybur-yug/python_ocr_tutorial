787fa9f1b48b242c9973b565fd818bd1397560ce
Michael Herman <hermanmu@gmail.com>
2015-01-12 14:16:22 -0700

    updated _app.sh

--- _app.sh
+++ _app.sh
@@ -3,8 +3,8 @@
 # Setup App
 wget https://github.com/rhgraysonii/ocr_tutorial/archive/v0.tar.gz
 tar -xf v0.tar.gz
-mv ocr_tutorial-0/* ../../home/
-cd ../../home
+mv ocr_tutorial-0/* ../home/
+cd ../home
 sudo apt-get install -y python-virtualenv
 virtualenv env
 source env/bin/activate
