6af4f84fa26935a24c2ffb6a43514a6fea1c8a1a
Robert H Grayson II <bobbygrayson@gmail.com>
2015-01-13 23:56:26 -0500

    only handle jpegs

--- flask_server/app.py
+++ flask_server/app.py
@@ -18,11 +18,14 @@ def main():
 def ocr():
     try:
         url = request.json['image_url']
-        output = process_image(url)
-        return jsonify({"output": output})
+        if 'jpg' in url:
+            output = process_image(url)
+            return jsonify({"output": output})
+        else:
+            return jsonify({"error": "only .jpg files, please"})
     except:
         return jsonify(
-            {"error": "Did you mean to send: {'image_url': 'some_url'}"}
+            {"error": "Did you mean to send: {'image_url': 'some_jpeg_url'}"}
         )
 
 
